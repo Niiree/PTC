d10f1a9a5d75e063919466f21c325814e528da27
unknown <nicolas.le-stunff@hotmail.fr>
2022-05-22 20:13:48 +0200

    update couverture de test + refactor Trie et Main

--- src/main/java/init/Trie.java
+++ src/main/java/init/Trie.java
@@ -3,12 +3,30 @@ package init;
 public class Trie {
 
 	
-public int[][] partition(int[] liste, Integer taille) {
+public int[][] partition(int[] liste, Integer taille) throws Exception {
+	
+	
+			
+		if(taille == null) {
+			throw new Exception("Impossible de diviser par 0 ou null");
+		}else if(taille == 0){
+			throw new Exception("Impossible de diviser par 0 ou null");
+		}
+		if(liste == null) {
+			throw new Exception ("Impossible de diviser une liste null");
+			
+		}
+		
 		
 		int listeLenght = liste.length; 
 		
+		if(listeLenght< taille) {
+			throw new Exception ("La taille doit être inférieur à la longueur de la liste");
+		}
+		
 		// Longueur maximum du tableau en comptant la possibilitée de nombre impaire
 		int listSize = (listeLenght/taille) + (listeLenght%taille); 
+		
 		
 		// Init du tableau 
 		int[][]result = new int[listSize][taille];
@@ -24,6 +42,7 @@ public int[][] partition(int[] liste, Integer taille) {
 				itterator2++;
 			}else {
 			// Sinon, on passe à la ligne suivante en réinitalisant l'itterator2 et incrémentant l'itterator1
+				
 				itterator1++;
 				itterator2=0;
 				result[itterator1][itterator2] = liste[i];
--- src/main/java/init/main.java
+++ src/main/java/init/main.java
@@ -9,7 +9,13 @@ public class Main {
 	Trie trie = new Trie();
 	int[] exemple ={1,2,3,4,5,6};
 	
-	System.out.print(Arrays.deepToString(trie.partition(exemple,2)));
+	//Voir le résultat de sortie
+	try {
+		System.out.print(Arrays.deepToString(trie.partition(exemple,2)));
+	} catch (Exception e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
 
 	
 	}
