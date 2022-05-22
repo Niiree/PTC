b0170d43377667c5bad1dbcda266d00d449ca26c
unknown <nicolas.le-stunff@hotmail.fr>
2022-05-22 19:00:39 +0200

    ajout de la première partie du dm

--- src/main/java/init/main.java
+++ src/main/java/init/main.java
@@ -1,10 +1,47 @@
 package init;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class main {
 	
 	public static void main (String[] args) {
+	
+	}
+
+
+	
+	
+	public int[][] partition(int[] liste, Integer taille) {
+		
+		int listeLenght = liste.length; 
+		
+		// Longueur maximum du tableau en comptant la possibilitée de nombre impaire
+		int listSize = (listeLenght/taille) + (listeLenght%taille); 
+		
+		// Init du tableau 
+		int[][]result = new int[listSize][taille-1];
+		
+		
+		int itterator1 = 0 ;
+		int itterator2 = 0 ;
+		
+		for (int i = 0;i<listeLenght;i++) {
+			// Si l'itterateur est < à la taille, récuperation de la valeur liste[i] et incrément de itterator2
+			if(itterator2 < taille) {
+				result[itterator1][itterator2] = liste[i];
+				itterator2++;
+			}else {
+			// Sinon, on passe à la ligne suivante en réinitalisant l'itterator2 et incrémentant l'itterator1
+				itterator1++;
+				itterator2=0;
+				result[itterator1][itterator2] = liste[i];
+				itterator2++;
+			}		
+		}
+		
+		return result;
+		
 		
 	}
 	
